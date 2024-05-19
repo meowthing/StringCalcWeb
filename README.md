d9d0286c0a0d9fd2a8cabeca5ee56e3a09aef575
Vinnie Leung <vleung20@gmail.com>
2024-05-19 15:58:52 +0100

    added all the packages - added mario striped background - added logo

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+# String Calculator Front-end
+Using: React/TS/Tailwind/ShadcnUI/Vite/Cypress
+Also: `vite-tsconfig-paths` to allow Vite to read tsconfig compiler option paths (so I don't need to define custom path resolution twice)
+
 # React + TypeScript + Vite
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
@@ -8,6 +12,7 @@ Currently, two official plugins are available:
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
 
 ## Expanding the ESLint configuration
+
 
 If you are developing a production application, we recommend updating the configuration to enable type aware lint rules:
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Input } from '@/components/ui/input'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div id="star-pattern">
+
+        <h1>Vinnie's fancy</h1>
+        <img src="./public/images/logo.png"></img>
+        <Input placeholder="Enter some numbers separated by commas e.g. '2,3'"></Input>
+        <div>
+          <span id="resultString">The total is 15!</span>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
