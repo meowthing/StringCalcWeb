9a7d600280dbe4f3ebe2f5384b33bbb2950858ca
Vinnie Leung <vleung20@gmail.com>
2024-05-20 16:02:03 +0100

    Speed Insights causing build errors, using GAnalytics instead

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import "./vl_globals.css";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <SpeedInsights />
     <App />
   </React.StrictMode>
 );
