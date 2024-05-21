02033c802c14272b8da1917e9e0777c7af175a7d
Vinnie Leung <vleung20@gmail.com>
2024-05-21 14:40:41 +0100

    forcing ports on vite preview, combining multiple start commands

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,10 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  server: {
+    strictPort: true,
+  },
+  preview: {
+    port: 5173,
+  },
 });
